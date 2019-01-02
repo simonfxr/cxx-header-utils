82473ef1af82364efb13e3964db739de00daca3f
Simon Reiser <simonfxr@gmail.com>
2019-01-02 20:39:25 +0100

    up

--- include/hu/macros.h
+++ include/hu/macros.h
@@ -57,6 +57,42 @@
 #define HU_NOINLINE
 #endif
 
+#if HU_CXX_11_P
+#define HU_NORETURN [[noreturn]]
+#else
+#if HU_COMP_GNULIKE_P || hu_has_attribute(noreturn)
+#define HU_NORETURN __attribute__((noreturn))
+#elif HU_COMP_MSVC_P
+#define HU_NORETURN
+#else
+#define HU_NORETURN
+#endif
+#endif
+
+#if HU_CXX_17_P
+#define HU_NODISCARD [[nodiscard]]
+#else
+#if HU_COMP_GNULIKE_P || hu_has_attribute(warn_unused_result)
+#define HU_NODISCARD __attribute__((warn_unused_result))
+#elif HU_COMP_MSVC_P && _MSC_VER >= 1700
+#define HU_NODISCARD _Check_return_
+#else
+#define HU_NODISCARD
+#endif
+#endif
+
+#if HU_CXX_14_P
+#define HU_DEPRECATED [[deprecated]]
+#else
+#if HU_COMP_GNULIKE_P || hu_has_attribute(deprecated)
+#define HU_DEPRECATED __attribute__((deprecated))
+#elif HU_COMP_MSVC_P
+#define HU_DEPRECATED __declspec(deprecated)
+#else
+#define HU_DEPRECATED
+#endif
+#endif
+
 #if HU_COMP_GNULIKE_P || hu_has_attribute(visibility)
 #define HU_LIB_EXPORT __attribute__((visibility("default")))
 #define HU_LIB_IMPORT HU_LIB_EXPORT
